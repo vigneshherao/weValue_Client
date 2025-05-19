36517c93585fe2e86e1d980bb3d54c6ab8a05c2e
vigneshherao <vigneshfornavy@gmail.com>
2025-05-19 18:56:59 +0530

    feat:implemented patner's section

--- wevalue_client/src/pages/landing/HomePage.jsx
+++ wevalue_client/src/pages/landing/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
 import HeroSection from "./HeroSection";
+import Patners from "./Patners";
 
 const HomePage = () => {
   return (
     <div className="w-full h-full">
       <HeroSection />
+      <Patners />
     </div>
   );
 };
---
+++ wevalue_client/src/pages/landing/Patners.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const Partners = () => {
+  const logos = [
+    "https://cdn.freebiesupply.com/images/large/2x/google-logo-transparent.png",
+    "https://cdn.freebiesupply.com/images/large/2x/google-logo-transparent.png",
+    "https://cdn.freebiesupply.com/images/large/2x/google-logo-transparent.png",
+    "https://cdn.freebiesupply.com/images/large/2x/google-logo-transparent.png",
+    "https://cdn.freebiesupply.com/images/large/2x/google-logo-transparent.png",
+    "https://cdn.freebiesupply.com/images/large/2x/google-logo-transparent.png",
+  ];
+
+  return (
+    <section className="py-10 px-4 sm:px-6 md:px-10 lg:px-20">
+      <h2 className="text-2xl md:text-3xl font-bold text-center text-[#0C0950]">
+        Our Partners
+      </h2>
+
+      <div className="mt-10">
+        <Marquee pauseOnHover={true} gradient={false} speed={40}>
+          <div className="flex items-center gap-8 sm:gap-10 md:gap-14 lg:gap-20">
+            {logos.map((logo, index) => (
+              <div
+                key={index}
+                className="w-24 sm:w-28 md:w-32  flex items-center justify-center"
+              >
+                <img
+                  src={logo}
+                  alt={`partner-${index}`}
+                  className="object-contain w-full h-full"
+                />
+              </div>
+            ))}
+          </div>
+        </Marquee>
+      </div>
+    </section>
+  );
+};
+
+export default Partners;
